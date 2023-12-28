9afb0f6a94222f1245448fa8409b5d139aed44e7
Marco <marco.kosters@hva.nl>
2023-12-28 16:49:14 +0100

    cooking
    hij snapt het eindelijk hoor

---
+++ scripts/script.js
@@ -0,0 +1,96 @@
+gsap.to("#l0", {
+    scrollTrigger: {
+        scrub: true,
+    },
+    y: -250,
+});
+
+gsap.to("#l0-2", {
+    scrollTrigger: {
+        scrub: true,
+    },
+    y: -200,
+});
+
+gsap.to("#l1", {
+    scrollTrigger: {
+        scrub: true,
+    },
+    y: -175,
+});
+
+gsap.to("#l2", {
+    scrollTrigger: {
+        scrub: true,
+    },
+    y: -150,
+});
+
+gsap.to("#l3", {
+    scrollTrigger: {
+        scrub: true,
+    },
+    y: -125,
+});
+
+gsap.to("#l4", {
+    scrollTrigger: {
+        scrub: true,
+    },
+    y: -100,
+});
+
+gsap.to("#l5", {
+    scrollTrigger: {
+        scrub: true,
+    },
+    y: -75,
+});
+
+gsap.to("#l6", {
+    scrollTrigger: {
+        scrub: true,
+    },
+    y: -50,
+});
+
+gsap.to(".h2", {
+    scrollTrigger: {
+        trigger: ".h2",
+        toggleActions: "restart pause reverse pause",
+        start: "top 75%",
+
+        scrub: true,
+        // markers: true,
+    },
+    x: '40%',
+});
+
+// gsap.from("#l0", {
+//     y: 50,
+//     duration: 1,
+// });
+// gsap.from("#l1", {
+//     y: 100,
+//     duration: 1,
+// });
+// gsap.from("#l2", {
+//     y: 150,
+//     duration: 1,
+// });
+// gsap.from("#l3", {
+//     y: 200,
+//     duration: 1,
+// });
+// gsap.from("#l4", {
+//     y: 250,
+//     duration: 1,
+// });
+// gsap.from("#l5", {
+//     y: 300,
+//     duration: 1,
+// });
+// gsap.from("#l6", {
+//     y: 350,
+//     duration: 1,
+// });
